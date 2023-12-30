3a8ed2aa922c8df58d30a39ed1640217de7315e0
abstractqqq <tq9695@gmail.com>
2023-12-30 13:23:27 -0500

    finally fixed all bugs

--- src/num/ols.rs
+++ src/num/ols.rs
@@ -42,35 +42,34 @@ fn pl_lstsq(inputs: &[Series]) -> PolarsResult<Series> {
     let add_bias = inputs[1].bool()?;
     let add_bias: bool = add_bias.get(0).unwrap();
     let ncols = inputs[2..].len() + add_bias as usize;
-    for s in inputs {
-        if s.null_count() > 0 || s.len() <= 1 {
+    // y = inputs[0], add_bias = inputs[1]
+    let y = inputs[0].f64()?;
+    let y = y.rechunk();
+    let y = y.to_ndarray()?.into_shape((nrows, 1)).unwrap();
+    let y = y.view().into_faer();
+    let mut vs: Vec<Series> = Vec::with_capacity(inputs.len() - 1);
+    for (i, s) in inputs.into_iter().enumerate() {
+        // Skip inputs[1] because it is just a boolean
+        if (i != 1) && (s.null_count() > 0 || s.len() <= 1) {
             let mut builder: ListPrimitiveChunkedBuilder<Float64Type> =
                 ListPrimitiveChunkedBuilder::new("betas", 1, ncols, DataType::Float64);
             let nan = vec![f64::NAN; ncols];
             builder.append_slice(&nan);
             let out = builder.finish();
             return Ok(out.into_series());
+        } else if i >= 2 {
+            let news = s
+                .rechunk()
+                .cast(&DataType::Float64)?
+                .with_name(&i.to_string());
+            vs.push(news);
         }
-    }
-
-    let mut vs: Vec<Series> = Vec::with_capacity(inputs.len() - 1);
-    for (i, s) in inputs[2..].into_iter().enumerate() {
-        let news = s
-            .rechunk()
-            .cast(&DataType::Float64)?
-            .with_name(&i.to_string());
-        vs.push(news);
     }
     // Constant term
     if add_bias {
         let one = Series::from_vec("const", vec![1_f64; nrows]);
         vs.push(one);
     }
-    // y
-    let y = inputs[0].f64()?;
-    let y = y.rechunk();
-    let y = y.to_ndarray()?.into_shape((nrows, 1)).unwrap();
-    let y = y.view().into_faer();
 
     let df_x = DataFrame::new(vs)?;
     // Copy data
